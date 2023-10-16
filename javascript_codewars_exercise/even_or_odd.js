5f7d09d588aa48ae2fe4f45f16e6e6512837b57e
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-16 23:14:09 +0200

    first solution

---
+++ javascript_codewars_exercise/even_or_odd.js
@@ -0,0 +1,11 @@
+/* function that takes an integer as an
+argument and returns "Even" foreven numbers
+or "Odd"for odd numbers.*/
+
+function evenOrOdd(number){
+    if (number % 2 === 0){
+        return 'Even'
+    } else{
+        return 'Odd'
+    }
+}
