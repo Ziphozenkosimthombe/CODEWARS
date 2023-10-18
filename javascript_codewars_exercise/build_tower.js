5799bb2bb8ae4c52b81e315f38e410e56ffbab6e
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-18 23:17:20 +0200

    task solutions

--- javascript_codewars_exercise/build_tower.js
+++ javascript_codewars_exercise/build_tower.js
@@ -1,4 +1,6 @@
-/* Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character. */
+/* Build a pyramid-shaped tower, as an array/list of strings,
+given a positive integer number of floors. A tower block is represented
+with "*" character. */
 function towerBuilder(nFloor){
     let space = ""
     let star = ""
