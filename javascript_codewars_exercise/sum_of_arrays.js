9e9ab15a1cfbcbb7f53575d45a9a7b22b659e11d
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-26 23:30:47 +0200

    task_solutions

---
+++ javascript_codewars_exercise/sum_of_arrays.js
@@ -0,0 +1,7 @@
+/*
+function that takes an array of numbers and returns the sum of the numbers. 
+The numbers can be negative or non-integer.
+If the array does not contain any numbers then you should return 0.
+*/
+
+let sum = (number) => number.reduce( (a,b) => a+b, 0)
