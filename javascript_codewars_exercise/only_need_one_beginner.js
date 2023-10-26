42cabb7ee9abab0ccd38d7ad061f85a2212b7925
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-27 00:10:28 +0200

    task_solutions

---
+++ javascript_codewars_exercise/only_need_one_beginner.js
@@ -0,0 +1,8 @@
+/*
+check whether the provided array contains the value.
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+const check = (a, x) => a.includes(x);
