d13fc8164a0ce3617c0df90d5cd4e665f87a20af
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-04 23:05:47 +0200

    task solutions

--- 8KYU/set_alarm.js
+++ 8KYU/set_alarm.js
@@ -4,12 +4,5 @@ function should return true if you are employed and not on vacation
 It should return false otherwise.
 */
 function setAlarm(employed, vacation){
-    if (employed === true && vacation === true)
-      return false
-    else if (employed === true && vacation === false)
-      return true
-    else if (employed === false && vacation === true)
-      return false
-    else if (employed === false && vacation === false )
-      return false
+    return (employed && !vacation) ? true : false;
   }
