5ca9d0b9b3a8e250cb8413cceaac828b30651dfc
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-04 23:19:24 +0200

    task solutions

---
+++ 8KYU/sum_the_strings.js
@@ -0,0 +1,7 @@
+/*
+ function that takes 2 integers in form of a string as an input,
+ and outputs the sum (also as a string):
+ */
+ function sumStr(a,b) {
+    return String(Number(a) + Number(b))
+  }
