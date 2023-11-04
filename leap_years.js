761952d9d704b3b7a82089435b35fb0364f2a8b0
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-04 21:16:21 +0200

    task solutions

---
+++ leap_years.js
@@ -0,0 +1,12 @@
+/*
+simply determine, whether a given year is a leap year or not.
+In case you don't know the rules, here they are:
+
+Years divisible by 4 are leap years,
+but years divisible by 100 are not leap years,
+but years divisible by 400 are leap years.
+ */
+
+function isLeapYear(year) {
+    return (year % 4 === 0) && ( (year % 100 !== 0) || (year % 400 === 0 ))
+}
