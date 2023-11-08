d784ab647ce17b59f714fb53da93594d9a2b6740
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-08 23:59:58 +0200

    task solutions

---
+++ javascript/7KYU/disemvowel_trolls.js
@@ -0,0 +1,9 @@
+/*
+function that takes a string and return a new string with all vowels removed.
+
+For example, the string
+"This website is for losers LOL!"would become "Ths wbst s fr lsrs LL!".
+*/
+function disemvowel(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
+}
