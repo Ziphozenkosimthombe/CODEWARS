a37f924fa2530e9362112e962009201931225876
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-15 11:10:13 +0200

    task_solutions

--- javascript/8KYU/ReturnNegative.js
+++ javascript/8KYU/ReturnNegative.js
@@ -2,4 +2,4 @@
 In this simple assignment you are given a number and have to make it negative.
 But maybe the number is already negative?
 */
-const makeNagative = num => (num > 0) ? -num : num; 
+const makeNagative = num => (num > 0) ? -num : num;
