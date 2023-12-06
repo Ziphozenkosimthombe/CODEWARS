d9f2fccfbc8c8ab3feb8f1428616b3dff54e4aa4
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-06 23:20:42 +0200

    task_solutions

--- javascript/8KYU/FakeBinary.js
+++ javascript/8KYU/FakeBinary.js
@@ -5,4 +5,18 @@ Return the resulting string.
 */
 function fakeBin(x){
       return x.split('').map( n => n < 5 ? 0 : 1).join('')
+}
+
+//another approuch
+function fakeBin(x){
+  var newStr = "";
+  for(var i=0;i<x.length;i++){
+    if(Number(x[i])>=5){
+      newStr += "1"
     }
+    else{
+      newStr += "0";
+    }
+  }
+  return newStr;
+}
