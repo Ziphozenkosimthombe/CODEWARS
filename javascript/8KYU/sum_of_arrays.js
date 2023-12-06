3bf3a7ef9799b97b09c094fd9541963267dc0383
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-06 21:53:30 +0200

    task_solutions

--- javascript/8KYU/sum_of_arrays.js
+++ javascript/8KYU/sum_of_arrays.js
@@ -5,3 +5,13 @@ If the array does not contain any numbers then you should return 0.
 */
 
 let sum = (numbers) => numbers.reduce( (a,b) => a+b, 0)
+
+
+// another solution or approuch
+function sum(numbers){
+    let sum = 0
+    for (i = 0; i < numbers.length; i++){
+        sum += numbers[i]
+    }
+    return sum
+}
