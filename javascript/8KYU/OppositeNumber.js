cca1387bfefe813139112a4ee63464bf2e350311
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-02-23 19:14:40 +0200

    codewars solutions

---
+++ javascript/8KYU/OppositeNumber.js
@@ -0,0 +1,12 @@
+/**
+ * Very simple, given a number, find its opposite.
+ * 
+*/ 
+
+//solution1
+function opposite(number) {
+    return -number
+}
+
+//solution2
+const opposite = number => -number
