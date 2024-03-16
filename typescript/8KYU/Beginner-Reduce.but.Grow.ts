12168f51fdc67cb7db5ff52d1eb17ac7bb1c6bfb
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-16 20:31:52 +0200

    codewars solution

---
+++ typescript/8KYU/Beginner-Reduce.but.Grow.ts
@@ -0,0 +1,11 @@
+/**
+ Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+ * */ 
+
+
+export function grow(arr: number[]): number{
+    let initialValue: number = 1;
+    return arr.reduce((acc, curr) => acc * curr, initialValue)
+}
