b67816bc8f404507c9ae74da9c8d04a47168abe2
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-03-29 20:43:26 +0200

    codewars solutions

---
+++ javascript/6KYU/Coding.Meetup.#11.js
@@ -0,0 +1,31 @@
+/*
+You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer.
+
+
+
+
+This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex.
+*/
+
+
+const getAverageAge = list => {
+    const totalAge = list.reduce((acc, developer) => acc + developer.age, 0)
+    const averageAge = totalAge / list.length
+    return Math.round(averageAge)
+}
+
+// Another Solution
+const getAverageAge2 = list => Math.round(list.reduce((acc, developer) => acc + developer.age, 0) / list.length)
